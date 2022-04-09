7372b092dfcb23df82bf6ccaf958749a5a682ae7
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-09 08:40:39 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char base_16;
+
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{
+		putchar(base_16);
+	}
+
+	for (base_16 = 93; base_16 <= 102; base_16++)
+	{
+		putchar(base_16);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
