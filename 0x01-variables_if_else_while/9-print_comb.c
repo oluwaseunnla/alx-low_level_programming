262e157cce4e5dde778a81e1f4dc6e9e7c42413b
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-09 09:11:57 -0700

    ok

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,13 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		
 		if (i == 57)
 		{
 			break;
 		}
 		putchar(',');
-		putchar('\n');
+		putchar(' ');
 	}
-
+	putchar('\n');
 	return (0);
 }
