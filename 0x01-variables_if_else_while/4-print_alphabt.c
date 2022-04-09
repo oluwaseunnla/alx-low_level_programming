525f6e93c8824a37e51c3d9bda0dcb7b03791fed
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-09 06:49:48 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q')
+		{
+			continue;
+		}
+		else if (i == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
