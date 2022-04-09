bcd1e39b78cfbe086b38688dd551473ef6247266
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-09 06:01:48 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a', i <= 'z', i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A', i <= 'Z', i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n);
+
+	return (0);
+}
