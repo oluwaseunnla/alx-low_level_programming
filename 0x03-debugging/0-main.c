252b14f61211079631bcd581b8b09f19de129f71
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-13 06:48:36 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - main Entry point
+ * positive_or_negative: gives a report of the int
  *Return: 0 (success)
  */
 
@@ -11,5 +12,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
