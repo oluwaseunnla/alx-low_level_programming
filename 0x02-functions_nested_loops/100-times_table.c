48e61be63dc25ccd8d4d4cc32d36a87d98e21150
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-12 18:53:34 -0700

    task 101

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 				_putchar('0' + (rep - 100) / 10);
 				_putchar('0' + rep % 10);
 			}
-		        if (b < n)
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
