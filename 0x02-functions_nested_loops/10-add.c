f31fcc69d73019b6d6806f1cf8ac5b02c6d8be74
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-12 17:55:11 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * add - entry point that adds 2 numbers
- * Returns: 0 (success)
- * @a: are number input parameter to the function
- * @b: is thr second input
+ *add - entry point that adds 2 numbers
+ *Returns: 0 (success)
+ *@a: are number input parameter to the function
+ *@b: is thr second input
  */
 
 int add(int a, int b)
 {
 	int result = a + b;
 
-	return(result);
+	return (result);
 }
