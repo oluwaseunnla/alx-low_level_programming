88b96ffcc4ca8c7e7b78247f8ee7e5b7d79372d9
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-14 09:56:40 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
 int main(void)
@@ -19,8 +18,6 @@ i = 0;
  * putchar(i);
  * }
  */
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
