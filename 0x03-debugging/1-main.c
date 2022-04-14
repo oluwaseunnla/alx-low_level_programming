c025d4c167af85cd097da5d27600527eab189e98
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-14 10:26:09 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,10 +15,10 @@ i = 0;
 /*
  * while (i < 10)
  * {
- * Infinite loop - No increase of variable
- * putchar(i);
+ * 	putchar(i);
  * }
  */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
