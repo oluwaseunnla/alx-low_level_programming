1f9aab7357959ec3fb2b01184a02fa833351043b
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-15 09:23:26 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: fist int to be multiplied
+ * @b: second int to be multiplied
+ * Return: value of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
