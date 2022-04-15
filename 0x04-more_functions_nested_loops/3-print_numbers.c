9e518fbbcb37f873b27ebba3252b8c43cfef95f8
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-15 09:29:28 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
