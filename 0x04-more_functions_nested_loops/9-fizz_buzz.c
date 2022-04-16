ecca3fb951f1a2b84f36e15050be0938993bb3b1
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-15 17:26:15 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("Fizz ");
@@ -21,7 +21,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
