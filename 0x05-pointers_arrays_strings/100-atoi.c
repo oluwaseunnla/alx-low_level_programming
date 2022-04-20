2c755c95bfea5d22f7931e03954ee50253a4f92c
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-20 16:39:08 -0700

    task 11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,16 +12,16 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	do{
-		if(s == '-')
+		if(*s == '-')
 			sign *= -1;
 
-		else if (s >= '0' && s <= '9')
-			num = (num * 10) + (s - 0);
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - 0);
 
 		else if (num > 0)
 			break;
 
-	} while (s++);
+	} while (*s++);
 
 	return (num * sign);
 }
