4ecb748c5d206b2d41aea257744bc5d49c7cc59e
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-20 09:12:17 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: parameter
+ * Retur: lenth of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < s[]; i++)
+	{
+
+	}
+	return (i);
+}
