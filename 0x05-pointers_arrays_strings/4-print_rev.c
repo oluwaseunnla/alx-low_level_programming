86c7ac2b0132282f45d2c265b2bf607fdf18b388
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-20 10:25:05 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,11 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\n')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for (i = i -1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
