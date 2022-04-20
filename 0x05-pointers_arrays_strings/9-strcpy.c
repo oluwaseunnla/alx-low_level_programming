dc6a27ae7cb763f85b59e2a8d9f049c15fd9928f
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-20 16:07:20 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - main entry
+ * @dest: first params
+ * @src: second params
+ */
+
+char *_stycpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
