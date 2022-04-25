a5fc5a2c00fdb868dfc4f61cfbcadf753c91f684
seunnla arokoyo <seunnlaarokoyo@gmail.com>
2022-04-25 15:38:00 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,7 @@ char *cap_string(char *str)
 	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (i == 0 && str[i] >= 97 && str[i] <= 122)
@@ -22,7 +23,7 @@ char *cap_string(char *str)
 		j = 0;
 		while (c[j] != '\0')
 		{
-			if (c[j] == str[i] && (str[i + 1] >= 97 &&str[i + 1] <= 122))
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
