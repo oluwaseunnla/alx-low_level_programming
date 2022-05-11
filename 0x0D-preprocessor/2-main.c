99789034f976e47eb1988b9fa7bf4dbbdfa49a5b
Oluwaseunnla <seunnlaarokoyo@gmail.com>
2022-05-11 14:27:42 -0700

    task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - write a program that prints the name of the file
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n" __FILE__);
+	return (0);
+}
