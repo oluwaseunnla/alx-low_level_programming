480982f39ef698407f3b57d767c2c936faadcfb5
Oluwaseunnla <seunnlaarokoyo@gmail.com>
2022-05-17 16:09:33 -0700

    task 3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 
 		if (i != n - 1 && separator != NULL)
-			print("%s", separator);
+			printf("%s", separator);
 	}
 	va_end(list);
 
