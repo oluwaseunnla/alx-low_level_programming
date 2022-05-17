f96cf3fb1de878e7bd66c5ec65e72c827708e893
Oluwaseunnla <seunnlaarokoyo@gmail.com>
2022-05-17 16:48:18 -0700

    task 4

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_int - print
+ * @list: arguments
+ */
+
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_float - print
+ * @list: arguments
+ */
+
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_char - print
+ * @list: arguments
+ */
+
+void print_char(va_list list)
+{
+	print("%c", va_arg(list, int));
+}
+
+/**
+ * print_str - print
+ * @list: arguments
+ */
+
+void print_str(va_list list)
+{
+	char *s = va_arg(list, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+}
+
+/**
+ * print_all - print any type
+ * @format: arguments to print
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0, j = 0;
+	char *sep = "";
+
+	printTypeStruct printType[] = {
+		{ "i", print_int },
+		{ "f", print_float },
+		{ "c", print_char },
+		{ "s", print_str },
+		{NULL, NULL}
+	};
+	va_start(list, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (*printType[j].type == format[i])
+			{
+				printf("%s", sep);
+				printType[j].printer(list);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
