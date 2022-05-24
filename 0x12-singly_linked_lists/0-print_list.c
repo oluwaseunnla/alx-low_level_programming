2565450024d949bc9a8a32f134476758fb658161
Oluwaseunnla <seunnlaarokoyo@gmail.com>
2022-05-24 14:36:14 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a link list
+ * @h: list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
