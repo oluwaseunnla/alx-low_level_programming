5039b5d351638ca2edf9307f51cac102de98476a
Oluwaseunnla <seunnlaarokoyo@gmail.com>
2022-05-25 07:03:31 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * bmain - function for me 
+ * bmain - function exectuted 
  * Return: no return.
  */
 
-void __attribute__ ((constructor)) bamin()
+void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
