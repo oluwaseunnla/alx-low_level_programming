cb8a766568f0d7d4909805ecbb9ec9e64f7d587c
Oluwaseunnla <seunnlaarokoyo@gmail.com>
2022-05-25 06:18:37 -0700

    task 6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * bmain - function
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bamin()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
